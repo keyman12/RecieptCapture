744e1b8224804d041bc52f1539937ba35e817ea8
David Key <Davidkey75@gmail.com>
2025-05-04 21:08:49 +0100

    Initial Commit

---
+++ Cam5/Cam5App.swift
@@ -0,0 +1,17 @@
+//
+//  Cam5App.swift
+//  Cam5
+//
+//  Created by David Key on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Cam5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cam5Tests/Cam5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Cam5Tests.swift
+//  Cam5Tests
+//
+//  Created by David Key on 04/05/2025.
+//
+
+import Testing
+@testable import Cam5
+
+struct Cam5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
